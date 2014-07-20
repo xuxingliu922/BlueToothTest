4d0e5c2cb0805d17b895dd92f11f5a9116c07f9a
yihao <yhchinabest@gmail.com>
2014-07-20 18:04:32 +0800

    Add UI in APP, Finish Arduino Code
    1 Add paired check
    2 Add Input and Output UI
    3 Add Serial Control Code on Arduino

--- BlueToothDemo/src/com/example/bluetoothdemo/MainActivity.java
+++ BlueToothDemo/src/com/example/bluetoothdemo/MainActivity.java
@@ -1,18 +1,50 @@
 package com.example.bluetoothdemo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	final BluetoothAdapter mBluetoothAdapter= BluetoothAdapter.getDefaultAdapter();
+	final String mTargetDeviceName = "Bluetooth_V3";
+	BluetoothDevice mTargetDevice = null;
+	BluetoothSocket mBluetoothSocket = null;
+	OutputStream mOSM = null;
+	InputStream mISM = null;
+	
+	private Button mBtn;
+	private EditText mEdt;
+	private TextView mTv;
+	private Handler mHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +55,60 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        mBtn = (Button)findViewById(R.id.btn);
+        mBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+		        if(null != mBluetoothSocket)
+		        {
+		        	writeToBluetooth(mEdt.getText().toString().getBytes());
+		        }	
+			}
+		});
+        mEdt = (EditText)findViewById(R.id.edt);
+        mTv = (TextView)findViewById(R.id.tV);
+        mHandler = new Handler()
+        {
+        	@Override
+        	public void handleMessage(Message msg)
+        	{
+        		mTv.setText((String)msg.obj);
+        	}
+        };
+        initBT();
+        Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				byte[] buffer = new byte[256];
+				try {
+					while(true)
+					{
+						int byteNum = mISM.read(buffer);
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						if(0 != byteNum)
+						{
+							Message msg = mHandler.obtainMessage();
+							msg.obj = new String(buffer);
+							mHandler.sendMessage(msg);
+						}
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+        t.start();
+        	
     }
 
 
@@ -61,5 +147,113 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
     }
+    
+	private void initBT() {
+
+		mBluetoothAdapter.enable();
+
+		BluetoothDevice device = findBoundDevice();
+		if (null != device) {
+			mTargetDevice = device;
+			createSocket();
+		} else {
+			mBluetoothAdapter.startDiscovery();
+		}
+
+		BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+			public void onReceive(Context context, Intent intent) {
+
+				String action = intent.getAction();
+				// ’“µΩ…Ë±∏
+				if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+					BluetoothDevice device = intent
+							.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+					if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+						Log.v("BT",
+								"find device:" + device.getName()
+										+ device.getAddress());
+						if (device.getName().equals(mTargetDeviceName)) {
+							mTargetDevice = intent
+									.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+							createSocket();
+						}
+
+					}
+				}
+			}
+		};
+
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter);
+		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+		registerReceiver(mReceiver, filter);
+	}
+    
+    private BluetoothDevice findBoundDevice()
+    {
+    	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    	// If there are paired devices
+    	if (pairedDevices.size() > 0) {
+    	    // Loop through paired devices
+    	    for (BluetoothDevice device : pairedDevices) {
+    	        // Add the name and address to an array adapter to show in a ListView
+    	        //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+    	    	if(device.getName().equals(mTargetDeviceName))
+    	    	{
+    	    		return device;
+    	    	}
+    	    }
+    	}
+    	return null;
+    }
+    
+	private void createSocket() {
+		// Get a BluetoothSocket to connect with the given BluetoothDevice
+		try {
+			// MY_UUID is the app's UUID string, also used by the server code
+			UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+			mBluetoothSocket = mTargetDevice
+					.createRfcommSocketToServiceRecord(uuid);
+
+			try {
+				// Connect the device through the socket. This will block
+				// until it succeeds or throws an exception
+				mBluetoothSocket.connect();
+				mOSM = mBluetoothSocket.getOutputStream();
+				mISM = mBluetoothSocket.getInputStream();
+			} catch (IOException connectException) {
+				// Unable to connect; close the socket and get out
+					mBluetoothAdapter.cancelDiscovery();
+			}
+		} catch (IOException e) 
+		{
+		}
+	}
+	
+	private void writeToBluetooth(byte[] bytes)
+	{
+		try {
+			mOSM.write(bytes);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void readfromBluetooth(byte[] bytes)
+	{
+		if(null != bytes)
+		{
+			try{
+				mISM.read(bytes);
+			}	
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
